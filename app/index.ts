e596e3fd5767166ae030370a858886e3d7810ad2
AntonPupin <toha4706@gmail.com>
2022-11-08 17:26:45 +0200

    (feat) 3.3 + 1.6

---
+++ app/index.ts
@@ -0,0 +1,9 @@
+import "./css/reset.css";
+import "./css/variables.css";
+import "./css/grid.css";
+import "./css/media-query.css";
+import "./css/grid-media-query.css";
+
+// import "./img/main-desktop.jpg";
+// import "./img/main-mobile.jpeg";
+// import "./img/main-table.jpg";
